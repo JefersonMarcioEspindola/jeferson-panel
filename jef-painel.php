81436a705b3a69ec62e00d70afc2f99c631a0e33
Jeferson <jefersonmarcio2011@gmail.com>
2026-05-21 23:02:58 -0300

    bump version to 1.0.1

--- jef-painel.php
+++ jef-painel.php
@@ -5,7 +5,7 @@
  * Description: Painel personalizado Jeferson.
  * Author: Jeferson Espindola
  * Author URI: https://jefersonespindola.com/
- * Version: 1.0.0
+ * Version: 1.0.1
  * Text Domain: jef-painel
  */
 
